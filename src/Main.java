b762582cc390a3c67e9de41ba18229aafb762d45
AkulGumudavall <gumudaau@mail.uc.edu>
2024-09-29 16:33:35 -0400

    fixed stuff

--- src/Main.java
+++ src/Main.java
@@ -2,27 +2,29 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("What is the cost of your item? ");
-        String trash = "";
+        //int cost = 0
         double cost = 0.00;
+        //Output "What is the cost of your item?"
+        System.out.println("What is the cost of your item? ");
         if (input.hasNextDouble()) {
+            //Input cost
             cost = input.nextDouble();
             input.nextLine();
+            //if cost < 100.00 then
+                //num shipping price = cost * 0.02
+                //Output "Your shipping price: $" + shipping_price + " ,And your total cost is: $" + (shipping_price + cost)
             if (cost < 100.00) {
                 double shipping_price = cost * 0.02;
                 System.out.println("Your shipping price: $" + shipping_price + " ,And your total cost is: $" + (shipping_price + cost));
 
-            } else {
+            }
+            //else
+                //Output "Your shipping price is: $0 , And your total cost is: $" + cost
+            else {
                 System.out.println("Your shipping price is: $0 , And your total cost is: $" + cost);
-            };
+            }
+            //end if
 
         }
-        else{
-            trash = input.nextLine();
-
-            System.out.println("\n You said your wage was:" + trash);
-            System.out.println("\n Please run the program again with the correct values");
-        }
-
     }
 }
